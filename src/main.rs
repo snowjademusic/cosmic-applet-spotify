bff5aacf0093c56f9c7d10fe309e10ffc7964d38
Shadi Vandeventer <shavdv@gmail.com>
2026-04-27 09:31:28 +0200

    fix(plz): work plz

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct PanelVisibility {
     show_artwork: bool,
 }
 
-// comment to trigger release again
+// comment to trigger release again(atp idek)
 
 impl Default for PanelVisibility {
     fn default() -> Self {
